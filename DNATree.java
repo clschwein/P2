676ef140b758bd1cf55be3d36fe655b162cde67f
clschwein <schwein@vt.edu>
2013-02-26 14:52:26 -0500

    Skeletons for files.

---
+++ DNATree.java
@@ -0,0 +1,16 @@
+/**
+ * DNA Tree class for Project 2.
+ * 
+ * @author Chris Schweinhart (schwein)
+ * @author Nate Kibler (nkibler7)
+ */
+public class DNATree {
+
+	// TODO Add member fields
+	
+	// TODO Add public interface methods
+	
+	// TODO Add private, recursive helper methods
+}
+
+// TODO Add private subclasses for DNATreeNode
